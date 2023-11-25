fac27bab8b2c3aa071f0cd7729102b7056101f5b
NathanMartin1 <138838405+NathanMartin1@users.noreply.github.com>
2023-11-25 13:36:59 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function hi() {  
+    document.getElementById("query").innerHTML = "Hello!";
+    document.getElementById("answer").innerHTML = "Hi there!";
+}
+function ib19() {
+    document.getElementById("query").innerHTML = "What is your name?";
+    document.getElementById("answer").innerHTML = "My name is InfoBot19, created to assist you with articles to protect you from COVID-19.";
+}
+function creator() {
+    document.getElementById("query").innerHTML = "Who created you?";
+    document.getElementById("answer").innerHTML = "My creator is Nathan Martin, an Australian amateur developer.";
+}
+function whatisit() {
+    document.getElementById("query").innerHTML = "What is COVID-19?";
+    document.getElementById("answer").innerHTML = "COVID-19 is a disease caused by a virus named SARS-CoV-2. It can be very contagious and spreads quickly. The COVID-19 pandemic is a global outbreak of coronavirus - an infectious disease caused by the severe acute respiratory syndrome. It can be traced back to Wuhan, China."
+}
+function ihavecovid19() {
+    document.getElementById("query").innerHTML = "I have COVID-19. What do I do?"
+    document.getElementById("answer").innerHTML  = "1. Self-Isolate.  2. Monitor your symptoms.  3. Contact your healthcare provider.  4. Follow medical advice.  5. Practise good hygiene.  6. Stay connected.  7. Contact emergency services if required."
+}
+function hygienetips() {
+    document.getElementById("query").innerHTML = "What are some good hygiene tips to protect myself?";
+    document.getElementById("answer").innerHTML = "1. Wash your hands frequently.  2. Avoid touching your face.  3. Wear a mask.  4. Practise respiratory etiquette.  5. Maintain social distancing.  6. Disinfect frequently touched surfaces.  7. Follow community guidelines."
+}
